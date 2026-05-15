851260d07916d58c2e70374664ea299b98b82abe
Jason Strang <jasondstrang@gmail.com>
2026-05-14 19:22:55 -0600

    testign pass

--- tests/selenium.test.js
+++ tests/selenium.test.js
@@ -29,7 +29,7 @@ test('Google search page loads', async () => {
         .build();
 
     try {
-        await driver.get('https://www.theimagineerdads.com');
+        await driver.get('https://www.theimagineerdad.com');
         let title = await driver.getTitle();
         expect(title).toContain('Imagineer');
         await takeScreenshot(driver, 'Google_search_page_loads');
